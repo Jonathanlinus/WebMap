e377da61c21cff0aafe428b2c2a556558dd324b2
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-08 00:03:01 +0330

    two coordinates added by for method

--- webmap.py
+++ webmap.py
@@ -3,7 +3,10 @@ import folium
 map = folium.Map(location=[33.902075,50.463142])
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[33.90,50.47], popup="Hi We are here", icon=folium.Icon(color='green')))
+
+for coordinates in [[33.90, 50.47], [33.92, 50.49]]:
+	fg.add_child (folium.Marker (location=coordinates, popup="Hi We are here", icon=folium.Icon ( color='green' ) ) )
+
 map.add_child(fg)
 
 
