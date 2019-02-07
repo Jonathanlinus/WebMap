d9dcf83acd270128476d57e27d77d28ea892bc58
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-07 23:56:53 +0330

    Feature Group Added

--- webmap.py
+++ webmap.py
@@ -1,5 +1,10 @@
 import folium
 
 map = folium.Map(location=[33.902075,50.463142])
-map.add_child(folium.Marker(location=[33.90,50.47], popup="Hi We are here", icon=folium.Icon(color='green')))
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[33.90,50.47], popup="Hi We are here", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
+
 map.save("location.html")
