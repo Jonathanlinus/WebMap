46fe3a6b043e5e6d9d324a01b0c4bdfc0f33d49b
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-07 23:43:42 +0330

    location fixed

--- webmap.py
+++ webmap.py
@@ -1,4 +1,4 @@
 import folium
 
-map = folium.Map(location=[33.902075,50.463142])
+map = folium.Map(location=[33.902075,50.463142], zoom_start=13, max_zoom=14, min_zoom=12)
 map.save("location.html")
