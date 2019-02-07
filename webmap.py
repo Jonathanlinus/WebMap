d8ecbaa57b4ba4e4b54c42b572b02699ffc886b9
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-07 23:54:41 +0330

    location fixed

--- webmap.py
+++ webmap.py
@@ -1,4 +1,5 @@
 import folium
 
-map = folium.Map(location=[33.902075,50.463142], zoom_start=13, max_zoom=14, min_zoom=12)
+map = folium.Map(location=[33.902075,50.463142])
+map.add_child(folium.Marker(location=[33.90,50.47], popup="Hi We are here", icon=folium.Icon(color='green')))
 map.save("location.html")
