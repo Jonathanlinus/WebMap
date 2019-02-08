a95c45482da261023ddb5c9e600ac355f3fb377f
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-09 00:38:06 +0330

    name of volcanoes added to popup

--- webmap.py
+++ webmap.py
@@ -4,13 +4,16 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+name = list(data["NAME"])
+
 
 map = folium.Map(location=[42.8799,-113.2210])
-fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-	fg.add_child (folium.Marker(location=[lt, ln], popup="Hi This is a Volcano", icon=folium.Icon ( color='green' )))
-	
+fg= folium.FeatureGroup(name="My Map")
+
+for lt, ln, na in zip(lat, lon, name):
+	fg.add_child ( folium.Marker( location=(lt, ln), popup=na, icon=folium.Icon ( color='green' ) ) )
+
 map.add_child(fg)
 
 
