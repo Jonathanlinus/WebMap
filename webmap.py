f69099d655cb93dc2d15638b04b85ce21457ef03
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-09 00:50:50 +0330

    diffrent collor doffrent height

--- webmap.py
+++ webmap.py
@@ -5,16 +5,24 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 name = list(data["NAME"])
+elev = list(data["ELEV"])
 
+def color_producter(elevation):
+	if elevation < 1000:
+		return 'blue'
+	elif 1000<= elevation < 3000:
+		return 'red'
+	else:
+		return 'green'
 
 map = folium.Map(location=[42.8799,-113.2210])
-
 fg= folium.FeatureGroup(name="My Map")
 
-for lt, ln, na in zip(lat, lon, name):
-	fg.add_child ( folium.Marker( location=(lt, ln), popup="Volcano Name: "+na, icon=folium.Icon ( color='green' ) ) )
+
+for lt, ln, na, el in zip(lat, lon, name, elev):
+	fg.add_child ( folium.Marker( location=(lt, ln), popup="Volcano Name: "+na +" & Height:"+str(el)+" m", icon=folium.Icon ( color=color_producter(el)) ) )
+
 
 map.add_child(fg)
-
 
 map.save("location.html")
