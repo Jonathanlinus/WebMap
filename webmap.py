d313006640353dcafde0578aea364d604bd9bed3
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-10 00:37:18 +0330

    layer control added

--- webmap.py
+++ webmap.py
@@ -33,5 +33,6 @@ fg.add_child ( folium.GeoJson ( open ( "world.json", 'r', encoding="utf-8-sig" )
                                 style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
                                 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'} ) )
 map.add_child ( fg )
+map.add_child(folium.LayerControl())
 
 map.save ( "location.html" )
